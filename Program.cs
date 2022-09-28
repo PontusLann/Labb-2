1d2213ec23433402a76f6d6065fff93186d7210f
PontusLann <pontus.lann@iths.se>
2022-09-28 15:53:04 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shapes;
+using System.Numerics;
+
+
+Circle newC = new Circle(new Vector2(3.0f, 4.0f), 2.0f);
+Console.WriteLine(newC);
+Rectangle newR = new Rectangle(new Vector2(2.0f, 1.0f), 5.0f);
+Console.WriteLine(newR);
+Sphere newS = new Sphere(new Vector3(4.0f, 3.0f, 5.0f), 3.5f);
+Console.WriteLine(newS);
+Cuboid newCu = new Cuboid(new Vector3(3.0f, 4.0f, 2.0f), new Vector3(3.0f, 3.0f, 3.0f));
+Console.WriteLine(newCu);
