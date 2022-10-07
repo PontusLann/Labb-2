1285f2de38e17a987b0050df1a3596480a360815
PontusLann <pontus.lann@iths.se>
2022-10-07 13:05:40 +0200

    fäedig

--- Program.cs
+++ Program.cs
@@ -10,21 +10,7 @@ for (int i = 0; i < shapes.Length; i++)
 {
     shapes[i] = Shape.GenerateShape();
     Console.WriteLine(shapes[i]);
-
 }
-Enum.GetNames(typeof(AllShapes)).OrderBy(x => x);
 Console.WriteLine();
 Shape.PrintValues();
 
-//Triangle Triangle = new Triangle(new Vector2(2.4f, 4.2f), new Vector2(1.7f, 7.4f), new Vector2(3.1f, 4.8f));
-//
-//Console.WriteLine(Triangle);
-
-
-//Shape.GenerateShape(new Vector3(2.0f, 3.0f, 2.2f));
-
-//for (int i = 0; i < shapes.Length; i++)
-//{
-//    shapes[i] = Shape.GenerateShape(new Vector3(3.0f, 2.4f, 6.0f));
-//    Console.WriteLine(shapes[i]);
-//}
