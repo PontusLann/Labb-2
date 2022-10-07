983fb40ff8b8c56102ff63d472708b0db4c6701c
PontusLann <pontus.lann@iths.se>
2022-10-07 09:58:03 +0200

    -

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
-﻿using Shapes;
+﻿
 using System.Numerics;
+using Shapes;
+using static Shapes.Shape;
+
+Shape[] shapes = new Shape[20];
 
 
-Circle newC = new Circle(new Vector2(3.0f, 4.0f), 2.0f);
-Console.WriteLine(newC);
-Rectangle newR = new Rectangle(new Vector2(2.0f, 1.0f), 5.0f);
-Console.WriteLine(newR);
-Sphere newS = new Sphere(new Vector3(4.0f, 3.0f, 5.0f), 3.5f);
-Console.WriteLine(newS);
-Cuboid newCu = new Cuboid(new Vector3(3.0f, 4.0f, 2.0f), new Vector3(3.0f, 3.0f, 3.0f));
-Console.WriteLine(newCu);
+for (int i = 0; i < shapes.Length; i++)
+{
+    shapes[i] = Shape.GenerateShape();
+    Console.WriteLine(shapes[i]);
+
+}
+Enum.GetNames(typeof(AllShapes)).OrderBy(x => x);
+Console.WriteLine();
+Shape.PrintValues();
+
+//Triangle Triangle = new Triangle(new Vector2(2.4f, 4.2f), new Vector2(1.7f, 7.4f), new Vector2(3.1f, 4.8f));
+//
+//Console.WriteLine(Triangle);
+
+
+//Shape.GenerateShape(new Vector3(2.0f, 3.0f, 2.2f));
+
+//for (int i = 0; i < shapes.Length; i++)
+//{
+//    shapes[i] = Shape.GenerateShape(new Vector3(3.0f, 2.4f, 6.0f));
+//    Console.WriteLine(shapes[i]);
+//}
